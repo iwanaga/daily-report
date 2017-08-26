65e55fd39fe04d41e87017a7bafe4d38e371bfe8
Yoshihiro Iwanaga <iwanaga.blackie@gmail.com>
2017-08-26 15:09:43 +0900

    generated by cli

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MdToolbarModule } from "@angular/material";
+import { ShowReportComponent } from './show-report/show-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowReportComponent
   ],
   imports: [
     BrowserModule,
