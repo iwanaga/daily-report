cf8a19f8833a5ae32d2bae9b835efd2f2abc3c3f
Yoshihiro Iwanaga <iwanaga.blackie@gmail.com>
2017-08-27 16:28:07 +0900

    レポート一覧画面のコンポーネント

--- client/src/app/show-report/show-report.component.ts
+++ client/src/app/show-report/show-report.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-report.component.css']
 })
 export class ShowReportComponent implements OnInit {
-
+  labels = ['Name', '8/25(金)', '8/24(木)', '8/23(水)'];
+  reportData = [
+    {
+      name: 'Adam Howard',
+      reports: [
+        {
+          value: 'aaa',
+          tasks: 'bbb',
+          message: 'ccc'
+        },
+        {
+          value: 'ddd',
+          tasks: 'eee',
+          message: 'fff'
+        },
+        {
+          value: 'ggg',
+          tasks: 'hhh',
+          message: 'iii'
+        }
+      ]
+    },
+    {
+      name: 'Eric Roberts',
+      reports: [
+        {
+          value: 'aaa',
+          tasks: 'bbb',
+          message: 'ccc'
+        },
+        {
+          value: 'ddd',
+          tasks: 'eee',
+          message: 'fff'
+        },
+        {
+          value: 'ggg',
+          tasks: 'hhh',
+          message: 'iii'
+        }
+      ]
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
   }
-
 }
+
