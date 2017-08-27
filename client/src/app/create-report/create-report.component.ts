229fdcaeef7c6427e4c78c787b3e05005dba8b47
Yoshihiro Iwanaga <iwanaga.blackie@gmail.com>
2017-08-28 00:15:48 +0900

    バインディング先のモデルを指定。デモ用のオートフィルボタンを追加

--- client/src/app/create-report/create-report.component.ts
+++ client/src/app/create-report/create-report.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-report.component.css']
 })
 export class CreateReportComponent implements OnInit {
-
+  report: Report = {
+    value: '',
+    tasks: ['','',''],
+    message: ''
+  };
   constructor() { }
 
   ngOnInit() {
   }
 
+  fill() {
+    this.report = {
+      value: 'Axionシステムのリリースが完了し、定常稼働開始。年間10億円の価値創出見込み。',
+      tasks: ['本番環境リリース完了', 'ユーザマニュアル完成', '運用手順書作成中\n・メンテナンス手順：done\n・障害対応手順：70%'],
+      message: '9/1(金)は全休予定です。'
+    }
+  }
+}
+export interface Report {
+  value: string;
+  tasks: string[];
+  message: string;
 }
