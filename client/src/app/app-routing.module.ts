dc383d818cbaeb64f54ab5202683c370c204cc7d
Yoshihiro Iwanaga <iwanaga.blackie@gmail.com>
2017-08-27 16:29:38 +0900

    レポート一覧コンポーネントへのルーティングを追記

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ShowReportComponent} from "./show-report/show-report.component";
 
 const routes: Routes = [
   {
     path: '',
     children: []
+  },
+  {
+    path: 'show',
+    component: ShowReportComponent
   }
 ];
 
