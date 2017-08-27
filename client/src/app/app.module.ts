e56989b7ae9e90d7a23f303aefc129f19d91bca3
Yoshihiro Iwanaga <iwanaga.blackie@gmail.com>
2017-08-27 22:15:52 +0900

    依存モジュールを追加

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MdGridListModule, MdToolbarModule} from "@angular/material";
+import {
+  MdButtonModule, MdCardModule, MdGridListModule, MdInputModule,
+  MdToolbarModule
+} from "@angular/material";
 import { ShowReportComponent } from './show-report/show-report.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { CreateReportComponent } from './create-report/create-report.component';
@@ -19,7 +22,10 @@ import { CreateReportComponent } from './create-report/create-report.component';
     BrowserAnimationsModule,
     AppRoutingModule,
     MdToolbarModule,
-    MdGridListModule
+    MdGridListModule,
+    MdInputModule,
+    MdButtonModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
